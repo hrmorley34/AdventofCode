1e07de145a408a552f40f2e34e3e09300bbb833f
hrmorley34 <henry@morley.org.uk>
2021-11-25 13:14:19 +0000

    2020-15.2, finally

--- 2020/Day15.py
+++ 2020/Day15.py
@@ -1,5 +1,5 @@
-def instances(array: list, obj: object) -> list[int]:
-    out = []
+def instances(array: list[int], obj: int) -> list[int]:
+    out: list[int] = []
     try:
         index = array.index(obj)
         while index >= 0:
@@ -12,13 +12,16 @@ def instances(array: list, obj: object) -> list[int]:
 
 PUZZLE_INPUT = [int(x) for x in input("> ").split(",")]
 
-arr = list(PUZZLE_INPUT)
-while len(arr) < 2020:
-    i = arr[-1]
-    if i in arr[:-1]:
-        *_, index2, index1 = instances(arr, i)
-        arr.append(index1 - index2)
+lastindexes = {i: instances(PUZZLE_INPUT, i)[-1] + 1 for i in set(PUZZLE_INPUT[:-1])}
+index = len(PUZZLE_INPUT)
+num = PUZZLE_INPUT[-1]
+while index <= 30000000:
+    if num not in lastindexes:
+        new_num = 0
     else:
-        arr.append(0)
-
-print(arr[2019])
+        new_num = index - lastindexes[num]
+    lastindexes[num] = index
+    if index in (2020, 30000000):
+        print(index, num)
+    index += 1
+    num = new_num
