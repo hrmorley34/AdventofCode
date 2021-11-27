4609a8e16da3cef5847a0193665110aaacff835b
hrmorley34 <henry@morley.org.uk>
2021-11-27 23:53:06 +0000

    2019-23 (Py), it is done
    Also type-hinted 2019-09, used in 2019-23

--- 2019/Day09.py
+++ 2019/Day09.py
@@ -1,33 +1,69 @@
+from __future__ import annotations
+
+from typing import Iterable
+
+
 # PART 1
-class Memory(list):
-    def __getitem__(self, y):
+
+
+class Memory(list[int]):
+    def __getitem__(self, y: int) -> int:
         assert y >= 0
-        try: return list.__getitem__(self, y)
-        except: return 0
-    def __setitem__(self, key, value):
+        try:
+            return list.__getitem__(self, y)
+        except:
+            return 0
+
+    def __setitem__(self, key: int, value: int):
         assert key >= 0
-        try: list.__setitem__(self, key, value)
+        try:
+            list.__setitem__(self, key, value)
         except:
             while len(self) < key:
                 self.append(0)
             self.append(int(value))
             assert self[key] == int(value)
-    def get_moded(self, key, mode, relative_base):
-        if mode == 0: return self[self[key]]
-        elif mode == 1: return self[key]
-        elif mode == 2: return self[relative_base + self[key]]
-        else: raise ValueError("Bad mode: {0}".format(mode))
-    def set_moded(self, key, value, mode, relative_base):
-        if mode == 0: self[self[key]] = value
-        elif mode == 1: self[key] = value
-        elif mode == 2: self[relative_base + self[key]] = value
-        else: raise ValueError("Bad mode: {0}".format(mode))
+
+    def get_moded(self, key: int, mode: int, relative_base: int) -> int:
+        if mode == 0:
+            return self[self[key]]
+        elif mode == 1:
+            return self[key]
+        elif mode == 2:
+            return self[relative_base + self[key]]
+        else:
+            raise ValueError("Bad mode: {0}".format(mode))
+
+    def set_moded(self, key: int, value: int, mode: int, relative_base: int):
+        if mode == 0:
+            self[self[key]] = value
+        elif mode == 1:
+            self[key] = value
+        elif mode == 2:
+            self[relative_base + self[key]] = value
+        else:
+            raise ValueError("Bad mode: {0}".format(mode))
+
 
 class IntcodeComputer:
-    def __init__(self, initial_memory, inputqueue=list(), outputqueue=list()):
+    memory: Memory
+    program_counter: int
+    queue: tuple[list[int], list[int]]
+    relative_base: int
+
+    def __init__(
+        self,
+        initial_memory: Iterable[int],
+        inputqueue: list[int] | None = None,
+        outputqueue: list[int] | None = None,
+    ):
         self.memory = Memory(initial_memory)
         self.program_counter = 0
-        self.queue = inputqueue, outputqueue
+        if inputqueue is None:
+            inputqueue = list()
+        if outputqueue is None:
+            outputqueue = list()
+        self.queue = (inputqueue, outputqueue)
         self.relative_base = 0
 
     def run_until_input(self):
@@ -35,78 +71,113 @@ class IntcodeComputer:
         while True:
             operation = memory[self.program_counter]
             modes = [int(x) for x in str(operation)[-3::-1]]
-            while len(modes) < 3: modes.append(0)
+            while len(modes) < 3:
+                modes.append(0)
             opcode = int(str(operation)[-2:])
-            if opcode == 1: # +
-                memory.set_moded(self.program_counter + 3,
-                         memory.get_moded(self.program_counter+1, modes[0], self.relative_base) \
-                       + memory.get_moded(self.program_counter+2, modes[1], self.relative_base),
-                                 modes[2], self.relative_base)
+            if opcode == 1:  # +
+                memory.set_moded(
+                    self.program_counter + 3,
+                    memory.get_moded(
+                        self.program_counter + 1, modes[0], self.relative_base
+                    )
+                    + memory.get_moded(
+                        self.program_counter + 2, modes[1], self.relative_base
+                    ),
+                    modes[2],
+                    self.relative_base,
+                )
                 self.program_counter += 4
-            elif opcode == 2: # *
-                memory.set_moded(self.program_counter + 3,
-                         memory.get_moded(self.program_counter+1, modes[0], self.relative_base) \
-                       * memory.get_moded(self.program_counter+2, modes[1], self.relative_base),
-                                 modes[2], self.relative_base)
+            elif opcode == 2:  # *
+                memory.set_moded(
+                    self.program_counter + 3,
+                    memory.get_moded(
+                        self.program_counter + 1, modes[0], self.relative_base
+                    )
+                    * memory.get_moded(
+                        self.program_counter + 2, modes[1], self.relative_base
+                    ),
+                    modes[2],
+                    self.relative_base,
+                )
                 self.program_counter += 4
-            elif opcode == 3: # IN
+            elif opcode == 3:  # IN
                 if len(self.queue[0]):
-                    memory.set_moded(self.program_counter + 1,
-                                     self.queue[0].pop(0),
-                                     modes[0], self.relative_base)
+                    memory.set_moded(
+                        self.program_counter + 1,
+                        self.queue[0].pop(0),
+                        modes[0],
+                        self.relative_base,
+                    )
                     self.program_counter += 2
                 else:
                     return
-            elif opcode == 4: # OUT
-                self.queue[1].append(memory.get_moded(self.program_counter + 1,
-                                                      modes[0], self.relative_base))
+            elif opcode == 4:  # OUT
+                self.queue[1].append(
+                    memory.get_moded(
+                        self.program_counter + 1, modes[0], self.relative_base
+                    )
+                )
                 self.program_counter += 2
-            elif opcode == 5: # JUMP IF NON-ZERO
-                if memory.get_moded(self.program_counter + 1,
-                                    modes[0], self.relative_base) != 0:
-                    self.program_counter = memory.get_moded(self.program_counter + 2,
-                                                            modes[1], self.relative_base)
+            elif opcode == 5:  # JUMP IF NON-ZERO
+                if (
+                    memory.get_moded(
+                        self.program_counter + 1, modes[0], self.relative_base
+                    )
+                    != 0
+                ):
+                    self.program_counter = memory.get_moded(
+                        self.program_counter + 2, modes[1], self.relative_base
+                    )
                 else:
                     self.program_counter += 3
-            elif opcode == 6: # JUMP IF ZERO
-                if memory.get_moded(self.program_counter + 1,
-                                    modes[0], self.relative_base) == 0:
-                    self.program_counter = memory.get_moded(self.program_counter + 2,
-                                                            modes[1], self.relative_base)
+            elif opcode == 6:  # JUMP IF ZERO
+                if (
+                    memory.get_moded(
+                        self.program_counter + 1, modes[0], self.relative_base
+                    )
+                    == 0
+                ):
+                    self.program_counter = memory.get_moded(
+                        self.program_counter + 2, modes[1], self.relative_base
+                    )
                 else:
                     self.program_counter += 3
-            elif opcode == 7: # <
-                if memory.get_moded(self.program_counter + 1,
-                                    modes[0], self.relative_base) \
-                 < memory.get_moded(self.program_counter + 2,
-                                    modes[1], self.relative_base):
+            elif opcode == 7:  # <
+                if memory.get_moded(
+                    self.program_counter + 1, modes[0], self.relative_base
+                ) < memory.get_moded(
+                    self.program_counter + 2, modes[1], self.relative_base
+                ):
                     v = 1
                 else:
                     v = 0
-                memory.set_moded(self.program_counter + 3,
-                                 v,
-                                 modes[2], self.relative_base)
+                memory.set_moded(
+                    self.program_counter + 3, v, modes[2], self.relative_base
+                )
                 self.program_counter += 4
-            elif opcode == 8: # ==
-                if memory.get_moded(self.program_counter + 1,
-                                    modes[0], self.relative_base) \
-                == memory.get_moded(self.program_counter + 2,
-                                    modes[1], self.relative_base):
+            elif opcode == 8:  # ==
+                if memory.get_moded(
+                    self.program_counter + 1, modes[0], self.relative_base
+                ) == memory.get_moded(
+                    self.program_counter + 2, modes[1], self.relative_base
+                ):
                     v = 1
                 else:
                     v = 0
-                memory.set_moded(self.program_counter + 3,
-                                 v,
-                                 modes[2], self.relative_base)
+                memory.set_moded(
+                    self.program_counter + 3, v, modes[2], self.relative_base
+                )
                 self.program_counter += 4
-            elif opcode == 9: # ADJUST REL_BASE
-                self.relative_base += memory.get_moded(self.program_counter + 1,
-                                                       modes[0], self.relative_base)
+            elif opcode == 9:  # ADJUST REL_BASE
+                self.relative_base += memory.get_moded(
+                    self.program_counter + 1, modes[0], self.relative_base
+                )
                 self.program_counter += 2
             elif opcode == 99:
                 return True
             else:
                 raise Exception(f"Unknown opcode at {self.program_counter}: {opcode}")
+
 
 if __name__ == "__main__":
     PUZZLE_INPUT = input("> ")
@@ -116,7 +187,7 @@ if __name__ == "__main__":
     assert i.run_until_input()
     print(i.queue[1])
 
-# PART 2
+    # PART 2
     i = IntcodeComputer(PUZZLE_INPUT_INTS, [2])
     assert i.run_until_input()
     print(i.queue[1])
--- 2019/Day23.py
+++ 2019/Day23.py
@@ -1,46 +1,95 @@
+from __future__ import annotations
+
 from Day09 import IntcodeComputer, Memory
+from typing import cast
+
+
+NAT_ID = 255
 
 
 # PART 1
-class NullComputer:
+class NotAlwaysTransmitting:
+    memory: Memory
+    queue: tuple[list[int], list[int]]
+
     def __init__(self):
         self.memory = Memory([])
         self.queue = ([], [])
 
+    def run_until_input(self):
+        pass
+
+    def double_y(self) -> bool:
+        return len(self.queue[1]) > 3 and self.queue[1][-1] == self.queue[1][-3]
+
 
 class Network:
-    def __init__(self, code, count, hardware=IntcodeComputer):
+    computers: dict[int, IntcodeComputer | NotAlwaysTransmitting]
+    idle: bool
+    last_nat_y: int = -1
+
+    def __init__(
+        self,
+        code: list[int],
+        count: int,
+        hardware: type[IntcodeComputer] = IntcodeComputer,
+    ):
         self.computers = {i: hardware(code) for i in range(count)}
         for i, computer in self.computers.items():
             computer.queue[0].append(i)
             computer.run_until_input()
+        self.computers[NAT_ID] = NotAlwaysTransmitting()
+        self.idle = False
 
     def run_once(self):
+        triggered = False
         for computer in self.computers.values():
-            if isinstance(computer, NullComputer):
+            if isinstance(computer, NotAlwaysTransmitting):
                 continue
+
             if len(computer.queue[0]) <= 0:
                 computer.queue[0].append(-1)
             computer.run_until_input()
-            while len(computer.queue[1]):
+            while len(computer.queue[1]) >= 3:
                 c = computer.queue[1].pop(0)
                 x = computer.queue[1].pop(0)
                 y = computer.queue[1].pop(0)
                 self.computers[c].queue[0].extend([x, y])
+                triggered = True
+        self.idle = not triggered
+        if self.idle:
+            nat = cast(NotAlwaysTransmitting, self.computers[255])
+            x, y = nat.queue[0][-2:]
+            nat.queue[1].extend([x, y])
+            self.computers[0].queue[0].extend([x, y])
 
-    def run_until_id(self, id=255):
-        if id not in self.computers:
-            self.computers[id] = NullComputer()
+    def run_until_nat(self) -> list[int]:
+        nat = cast(NotAlwaysTransmitting, self.computers[NAT_ID])
         iterations = 0
-        while len(self.computers[id].queue[0]) <= 0:
+        while len(nat.queue[0]) <= 0:
             self.run_once()
             iterations += 1
             if iterations % 20000 == 0:
-                print(iterations)
-        return self.computers[id].queue[0]
+                print(":", iterations)
+        return nat.queue[0]
+
+    def run_until_double_y(self) -> int:
+        nat = cast(NotAlwaysTransmitting, self.computers[NAT_ID])
+        iterations = 0
+        while not nat.double_y():
+            self.run_once()
+            iterations += 1
+            if iterations % 20000 == 0:
+                print(":", iterations)
+        return nat.queue[1][-1]
 
 
-PUZZLE_INPUT = [int(i) for i in input("> ").split(",")]
+if __name__ == "__main__":
+    PUZZLE_INPUT = [int(i) for i in input("> ").split(",")]
 
-n = Network(PUZZLE_INPUT, 50)
-v = n.run_until_id()[1]
+    n = Network(PUZZLE_INPUT, 50)
+    v = n.run_until_nat()[1]
+    print("Part 1:", v)
+
+    v2 = n.run_until_double_y()
+    print("Part 2:", v2)
