e637e9f034e85e0a04f0432add68d58f269b8034
hrmorley34 <henry@morley.org.uk>
2021-12-01 06:59:17 +0000

    2021-01 (C#)

---
+++ 2021/Day01cs/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Day01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>();
+            string line;
+            while ((line = Console.ReadLine()) != null)
+            {
+                numbers.Add(Convert.ToInt32(line));
+            }
+
+            Console.WriteLine($"Part 1: {SumGts(numbers)}");
+            Console.WriteLine($"Part 1: {SumGts(GroupArgs(numbers))}");
+        }
+
+        static int SumGts(IEnumerable<int> collection)
+        {
+            int total = 0;
+            int lasti = collection.First();
+            foreach (int item in collection.Skip(1))
+            {
+                if (item > lasti) total++;
+                lasti = item;
+            }
+            return total;
+        }
+
+        static List<int> GroupArgs(IEnumerable<int> collection)
+        {
+            List<int> active = new List<int>() { collection.First(), collection.Skip(1).First(), 0 };
+            List<int> groups = new List<int>();
+            foreach (int item in collection.Skip(2))
+            {
+                for (int i = 0; i < active.Count; i++)
+                {
+                    active[i] += item;
+                }
+                groups.Add(active[0]);
+                active.RemoveAt(0);
+                active.Add(0);
+            }
+            return groups;
+        }
+    }
+}
