c915110828a328df3dbbad9416752d308b30be26
hrmorley34 <henry@morley.org.uk>
2021-12-14 07:42:23 +0000

    2021-14.2 (Py)

--- 2021/Day14.py
+++ 2021/Day14.py
@@ -16,6 +16,30 @@ def polymerise(
     return polymer
 
 
+def polymerise_by_neighbour(
+    polymer: Counter[tuple[PolymerPart, PolymerPart]],
+    mapping: dict[tuple[PolymerPart, PolymerPart], PolymerPart],
+) -> Counter[tuple[PolymerPart, PolymerPart]]:
+    new_polymer: Counter[tuple[PolymerPart, PolymerPart]] = Counter()
+    for (keyl, keyr), value in polymer.items():
+        keymiddle = mapping[keyl, keyr]
+        new_polymer[keyl, keymiddle] += value
+        new_polymer[keymiddle, keyr] += value
+    return new_polymer
+
+
+def polymer_neighbour_to_count(
+    polymer: Counter[tuple[PolymerPart, PolymerPart]],
+    start: PolymerPart,
+    end: PolymerPart,
+) -> Counter[PolymerPart]:
+    c: Counter[PolymerPart] = Counter((start, end))
+    for key in polymer:
+        for p in key:
+            c[p] += polymer[key]
+    return Counter({k: v // 2 for k, v in c.items()})
+
+
 if __name__ == "__main__":
     PUZZLE_INPUT = puzzle_input()
     basepolymer, lines = PUZZLE_INPUT.split("\n\n")
@@ -24,9 +48,24 @@ if __name__ == "__main__":
         (PolymerPart(a[0]), PolymerPart(a[1])): PolymerPart(b)
         for a, b in map(lambda s: s.split(" -> "), lines.splitlines())
     }
-    polymer = POLYMER
+    POLYMERCOUNT = Counter(
+        (POLYMER[x], POLYMER[x + 1]) for x in range(len(POLYMER) - 1)
+    )
+
+    # polymer = POLYMER
+    # for _ in range(10):
+    #     polymer = polymerise(polymer, MAPPING)
+    # counts = Counter(polymer)
+
+    polymer = POLYMERCOUNT
     for _ in range(10):
-        polymer = polymerise(polymer, MAPPING)
-    counts = Counter(polymer)
+        polymer = polymerise_by_neighbour(polymer, MAPPING)
+    counts = polymer_neighbour_to_count(polymer, POLYMER[0], POLYMER[-1])
     order = counts.most_common()
     print("Part 1:", order[0][1] - order[-1][1])
+
+    for _ in range(40 - 10):
+        polymer = polymerise_by_neighbour(polymer, MAPPING)
+    counts = polymer_neighbour_to_count(polymer, POLYMER[0], POLYMER[-1])
+    order = counts.most_common()
+    print("Part 2:", order[0][1] - order[-1][1])
