cd7141795aa915e374a4bddf5eebf7203f592dde
hrmorley34 <henry@morley.org.uk>
2023-12-05 05:59:28 +0000

    2023-05.2 (Py)

--- 2023/Day05.py
+++ 2023/Day05.py
@@ -1,6 +1,18 @@
 from dataclasses import dataclass
+from typing import Iterable
 
 from puzzle_input import puzzle_input
+
+
+@dataclass
+class Range2:
+    start: int
+    length: int
+
+    @classmethod
+    def from_seeds(cls, nums: Iterable[int]) -> Iterable["Range2"]:
+        nums = list(nums)
+        return (Range2(*pair) for pair in zip(nums[::2], nums[1::2]))
 
 
 @dataclass
@@ -17,6 +29,26 @@ class MapLine:
         if self.src <= value < self.src + self.range:
             value += -self.src + self.dest
         return value
+
+    def map_r(self, value: Range2) -> tuple[Range2 | None, list[Range2]]:
+        if self.src + self.range <= value.start:
+            return (None, [value])
+        elif value.start + value.length <= self.src:
+            return (None, [value])
+
+        left = value.start
+        right = value.start + value.length
+        outer_ranges: list[Range2] = []
+        if left < self.src:
+            new_left = self.src
+            outer_ranges.append(Range2(left, new_left - left))
+            left = new_left
+        if right > self.src + self.range:
+            new_right = self.src + self.range
+            outer_ranges.append(Range2(new_right, right - new_right))
+            right = new_right
+        middle_range = Range2(left - self.src + self.dest, right - left)
+        return middle_range, outer_ranges
 
 
 @dataclass
@@ -40,6 +72,20 @@ class Mapping:
                 return new
         return value
 
+    def map_r(self, values: list[Range2]) -> Iterable[Range2]:
+        values = values.copy()
+        for m in self.maps:
+            new_values: list[Range2] = []
+            while values:
+                middle, sides = m.map_r(values.pop())
+                if middle is not None:
+                    yield middle
+                new_values.extend(sides)
+            values = new_values
+            if not values:
+                return  # mapped everything
+        yield from values  # unchanged
+
 
 if __name__ == "__main__":
     PUZZLE_INPUT = puzzle_input().split("\n\n")
@@ -48,12 +94,19 @@ if __name__ == "__main__":
 
     assert seeds.startswith("seeds: "), seeds
     seeds = seeds[len("seeds: ") :]
-    stat_i = map(int, seeds.split())
 
+    stat_i = map(int, seeds.split())
     for m in mappings:
         stat_i = map(m.map, stat_i)
         # stat_i = list(stat_i)
         # print(stat_i)
-
     locs = list(stat_i)
     print("Part 1:", min(locs))
+
+    stat_i = list(Range2.from_seeds(map(int, seeds.split())))
+    # print(stat_i)
+    for m in mappings:
+        stat_i = list(m.map_r(stat_i))
+        # print(stat_i)
+    locs = stat_i
+    print("Part 2:", min(r.start for r in locs))
