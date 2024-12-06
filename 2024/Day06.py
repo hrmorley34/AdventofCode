6b66fcbce729eda0725f977195fa786abf98a04b
hrmorley34 <henry@morley.org.uk>
2024-12-06 05:26:57 +0000

    2024-06.2 (Py)

--- 2024/Day06.py
+++ 2024/Day06.py
@@ -7,13 +7,22 @@ START_CHAR = "^"
 START_DIR = 3
 
 
-def explore_map(map: list[str]) -> Generator[tuple[int, int], None, None]:
-    ((y, start_row),) = ((i, r) for i, r in enumerate(map) if START_CHAR in r)
-    x = start_row.index(START_CHAR)
-    dir = START_DIR
+def explore_map(
+    map: list[str],
+    start_pos: tuple[int, int] | None = None,
+    start_dir: int | None = None,
+) -> Generator[tuple[tuple[int, int], int], None, None]:
+    if start_pos is None:
+        ((y, start_row),) = ((i, r) for i, r in enumerate(map) if START_CHAR in r)
+        x = start_row.index(START_CHAR)
+    else:
+        x, y = start_pos
+    if start_dir is None:
+        dir = START_DIR
+    else:
+        dir = start_dir % len(DIRECTIONS)
 
     while 0 <= y < len(map) and 0 <= x < len(map[0]):
-        yield (x, y)
         dx, dy = DIRECTIONS[dir]
         while (
             0 <= x + dx < len(map)
@@ -22,10 +31,44 @@ def explore_map(map: list[str]) -> Generator[tuple[int, int], None, None]:
         ):
             dir = (dir + 1) % len(DIRECTIONS)
             dx, dy = DIRECTIONS[dir]
+
+        yield (x, y), dir
+
         x, y = x + dx, y + dy
 
 
 if __name__ == "__main__":
     PUZZLE_INPUT = puzzle_input().splitlines()
 
-    print("Part 1:", len(set(explore_map(PUZZLE_INPUT))))
+    positions = set[tuple[int, int]]()
+    pos_dirs = set[tuple[tuple[int, int], int]]()
+    loop_creators = set[tuple[int, int]]()
+    for (x, y), dir in explore_map(PUZZLE_INPUT):
+        positions.add((x, y))
+        pos_dirs.add(((x, y), dir))
+
+        dx, dy = DIRECTIONS[dir]
+        if (
+            0 <= x + dx < len(PUZZLE_INPUT)
+            and 0 <= y + dy < len(PUZZLE_INPUT[0])
+            and PUZZLE_INPUT[y + dy][x + dx] != "#"
+            # and we insert a wall here
+            and (x + dx, y + dy) not in positions
+            # haven't previously passed through this space
+        ):
+            new_map = [
+                "".join(
+                    "#" if ix == x + dx and iy == y + dy else c
+                    for ix, c in enumerate(line)
+                )
+                for iy, line in enumerate(PUZZLE_INPUT)
+            ]
+            sub_pos_dirs = set[tuple[tuple[int, int], int]]()
+            for pd in explore_map(new_map, (x, y), dir):
+                if pd in sub_pos_dirs:
+                    loop_creators.add((x + dx, y + dy))
+                    break
+                sub_pos_dirs.add(pd)
+
+    print("Part 1:", len(positions))
+    print("Part 2:", len(loop_creators))
