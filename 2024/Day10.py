8842d63c4cce58c33ec76a0e54d722ea954ac762
hrmorley34 <henry@morley.org.uk>
2024-12-10 05:12:47 +0000

    2024-10.2 (Py)

--- 2024/Day10.py
+++ 2024/Day10.py
@@ -3,22 +3,25 @@ from puzzle_input import puzzle_input
 NEIGHBOURS = [(1, 0), (0, 1), (-1, 0), (0, -1)]
 
 
-def explore_head(map: list[list[int]], head: tuple[int, int]) -> set[tuple[int, int]]:
+def explore_head(
+    map: list[list[int]], head: tuple[int, int]
+) -> set[tuple[tuple[int, int], ...]]:
     height = map[head[1]][head[0]]
-    positions: set[tuple[int, int]] = {head}
+    paths: set[tuple[tuple[int, int], ...]] = {(head,)}
     while height < 9:
         height += 1
-        new_positions: set[tuple[int, int]] = set()
-        for x, y in positions:
+        new_paths: set[tuple[tuple[int, int], ...]] = set()
+        for path in paths:
+            x, y = path[-1]
             for dx, dy in NEIGHBOURS:
                 if (
                     0 <= x + dx < len(map[0])
                     and 0 <= y + dy < len(map)
                     and map[y + dy][x + dx] == height
                 ):
-                    new_positions.add((x + dx, y + dy))
-        positions = new_positions
-    return positions
+                    new_paths.add(path + ((x + dx, y + dy),))
+        paths = new_paths
+    return paths
 
 
 if __name__ == "__main__":
@@ -27,6 +30,15 @@ if __name__ == "__main__":
 
     print(
         "Part 1:",
+        sum(
+            len({path[-1] for path in explore_head(MAP, (x, y))})
+            for y, line in enumerate(MAP)
+            for x, i in enumerate(line)
+            if i == 0
+        ),
+    )
+    print(
+        "Part 2:",
         sum(
             len(explore_head(MAP, (x, y)))
             for y, line in enumerate(MAP)
