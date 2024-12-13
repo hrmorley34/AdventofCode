a60ca6bf51b69e0912cf964c6b8a1e92b710ee7c
hrmorley34 <henry@morley.org.uk>
2024-12-13 06:17:16 +0000

    2024-13.2 (Py)

--- 2024/Day13.py
+++ 2024/Day13.py
@@ -1,5 +1,6 @@
 import re
 from dataclasses import dataclass
+from fractions import Fraction
 from typing import Self
 
 from puzzle_input import puzzle_input
@@ -9,6 +10,8 @@ CLAW_PATTERN = re.compile(
     r"Button B: X\+(\d+), Y\+(\d+)\n"
     r"Prize: X=(\d+), Y=(\d+)"
 )
+
+OFFSET2 = 10000000000000
 
 
 @dataclass
@@ -27,17 +30,28 @@ class ClawMachine:
             prize=(int(m[5]), int(m[6])),
         )
 
+    @classmethod
+    def from_string2(cls, s: str) -> Self:
+        m = CLAW_PATTERN.fullmatch(s.strip())
+        assert m is not None
+        return cls(
+            a=(int(m[1]), int(m[2])),
+            b=(int(m[3]), int(m[4])),
+            prize=(int(m[5]) + OFFSET2, int(m[6]) + OFFSET2),
+        )
+
     def solve(self) -> tuple[int, int] | None:
-        a = min(self.prize[1] // self.a[1], 100)
-        b = (self.prize[1] - a * self.a[1]) // self.b[1]
-        while (
-            self.a[0] * a + self.b[0] * b != self.prize[0]
-            or self.a[1] * a + self.b[1] * b != self.prize[1]
-        ):
-            a -= 1
-            b = (self.prize[1] - a * self.a[1]) // self.b[1]
-            if a < 0 or b > 100:
-                return None
+        ax, ay = self.a
+        bx, by = self.b
+        tx, ty = self.prize
+        b = (tx - Fraction(ty, ay) * ax) / (bx - Fraction(by, ay) * ax)
+        a = Fraction(tx - b * bx, ax)
+        if a.denominator != 1 or b.denominator != 1:
+            # no integer solution
+            return None
+        a = a.numerator
+        b = b.numerator
+        assert a >= 0 and b >= 0
         return a, b
 
     def cost(self) -> int:
@@ -47,6 +61,9 @@ class ClawMachine:
 
 if __name__ == "__main__":
     PUZZLE_INPUT = puzzle_input().split("\n\n")
-    MACHINES = list(map(ClawMachine.from_string, PUZZLE_INPUT))
 
+    MACHINES = list(map(ClawMachine.from_string, PUZZLE_INPUT))
     print("Part 1:", sum(c.cost() for c in MACHINES))
+
+    MACHINES2 = list(map(ClawMachine.from_string2, PUZZLE_INPUT))
+    print("Part 2:", sum(c.cost() for c in MACHINES2))
